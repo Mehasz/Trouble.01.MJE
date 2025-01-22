d8e588a82c0621fecab8cc829743d74a108ac571
Mehasz <tup83552@temple.edu>
2025-01-22 13:40:04 -0500

    Update script.js
    all fixed!

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 //↓↓↓↓↓↓↓↓↓↓↓↓TROUBLESHOOT the code  below here↓↓↓↓↓↓↓↓↓↓↓↓
 const mySuperNeatoObject = {
-    type: "gadget"
-    hasWiFi: false
-    }
+  type: "gadget",
+  hasWiFi: false,
+};
 
-}
 //↑↑↑↑↑↑↑↑↑↑TROUBLESHOOT the code above here↑↑↑↑↑↑↑↑↑↑
 //Do not edit below this comment!
-
-
-
-
